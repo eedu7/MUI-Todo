be1a3b5f74574e39ab5d0f4114c301f004f94909
eedu7 <mueedahmad067@gmail.com>
2025-06-14 10:08:59 +0500

    Created the react project and added tailwindcss and prettier

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+
+function App() {
+
+  return (
+    <main className="min-h-screen mx-auto max-w-7xl flex justify-center py-32 bg-zinc-100">
+        <h1 className="text-4xl font-bold text-shadow-amber-500 text-amber-400 font-serif tracking-wider">Hello World</h1>
+    </main>
+  )
+}
+
+export default App
