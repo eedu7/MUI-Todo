6e03727d0b9c5f0123fb8a8cf211a3032e683186
eedu7 <mueedahmad067@gmail.com>
2025-06-18 11:49:44 +0500

    Added colors

--- src/components/TodoListCards.tsx
+++ src/components/TodoListCards.tsx
@@ -40,13 +40,28 @@ interface TodoCardProps {
     created_at: string;
 }
 
-const TodoCard = ({ title, description }: TodoCardProps) => {
+const TodoCard = ({ title, description, status }: TodoCardProps) => {
+    const statusColors = {
+        pending: {
+            bg: "#FFF3CD",
+            text: "#664d03",
+        },
+        "in-progress": {
+            bg: "#D1ECF1",
+            text: "#0c5460",
+        },
+        completed: {
+            bg: "#D4EDDA",
+            text: "#155724",
+        },
+    };
     return (
         <Grid
             size={{
                 xs: 12,
                 sm: 6,
-                md: 3,
+                md: 4,
+                lg: 3,
             }}
         >
             <Card
@@ -54,11 +69,16 @@ const TodoCard = ({ title, description }: TodoCardProps) => {
                 sx={{
                     width: "100%",
                     height: "100%",
+                    backgroundColor: statusColors[status].bg,
+                    color: statusColors[status].text,
+                    borderRadius: 2,
+                    border: "1px solid rgba(0, 0, 0, 0.05)",
+                    boxShadow: "0 2px 6px rgba(0, 0, 0, 0.05)",
                 }}
             >
                 <CardContent>
-                    <Typography>{title}</Typography>
-                    <Typography>{description}</Typography>
+                    <Typography variant="h6">{title}</Typography>
+                    <Typography variant="body2">{description}</Typography>
                 </CardContent>
             </Card>
         </Grid>
